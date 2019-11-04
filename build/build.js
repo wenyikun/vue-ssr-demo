f8e678e72c5aaf9716e21636289003625b43d598
wenyikun <wenyikun86@gmail.com>
2019-11-04 16:11:56 +0800

    自动刷新配置

--- build/build.js
+++ build/build.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
+// const webpackDevMiddleware = require('webpack-dev-middleware')
 
 const app = express()
-const config = require('./webpack.base.config')
+// const config = require('./webpack.base.config')
 const clientCompiler = webpack(require('./webpack.client.config'))
 const serverCompiler = webpack(require('./webpack.server.config'))
+
+// app.use(webpackDevMiddleware(clientCompiler, {
+//   publicPath: config.output.publicPath
+// }));
 
 clientCompiler.watch({ aggregateTimeout: 300, poll: 1000 }, (err, stats) => {
   console.log(
